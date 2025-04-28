d281d1ae25247a52cb7459bdbb8d59c9fa447a83
jmacd229 <jessemacdgl@gmail.com>
2025-04-27 23:29:02 -0400

    Adds initial configuration
    - Configures vite and tsconfig files
    - Adds font families

--- apps/jesse-website/vite.config.ts
+++ apps/jesse-website/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: __dirname,
@@ -15,6 +16,16 @@ export default defineConfig({
     host: 'localhost',
   },
   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  resolve: {
+    alias: [
+      {
+        find: 'design-system',
+        replacement: fileURLToPath(
+          new URL('../../libs/design-system', import.meta.url)
+        ),
+      },
+    ],
+  },
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],

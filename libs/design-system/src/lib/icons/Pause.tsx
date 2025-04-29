407ea5794122138d9de584b543dd6d4855089cc7
jmacd229 <jessemacdgl@gmail.com>
2025-04-28 22:27:38 -0400

    Fixes animation pause/play functionality
    - Previously the animation was being destroyed and re-created whenever it was paused via the toggle. This is not fixed by splitting the use-effect
    - Also fixed incorrect usage of setSegments instead of goToAndStop
    - Fixed React warnings about SVG properties being snake-case

--- libs/design-system/src/lib/icons/Pause.tsx
+++ libs/design-system/src/lib/icons/Pause.tsx
@@ -5,9 +5,9 @@ export default () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="1"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="1"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     xmlns="http://www.w3.org/2000/svg"
   >
     <rect x="6" y="4" width="4" height="16" />
